c9b1b993a6da77829c1258ccd3756e2d8fde77fc
Tom Moulton <tom@rnode.moulton.us>
2025-02-02 18:47:36 -0500

    update crypto to add category

--- inventory/manage/item_categories.php
+++ inventory/manage/item_categories.php
@@ -225,16 +225,16 @@ else
 
 check_row(_("Exclude from purchases:"), 'no_purchase');
 
-gl_all_accounts_list_row(_("Sales Account:"), 'sales_account', $_POST['sales_account']);
-
 if (is_service($_POST['mb_flag']))
 {
+	gl_all_accounts_list_row(_("Sales Account:"), 'sales_account', $_POST['sales_account']);
 	gl_all_accounts_list_row(_("C.O.G.S. Account:"), 'cogs_account', $_POST['cogs_account']);
 	hidden('inventory_account', $_POST['inventory_account']);
 	hidden('adjustment_account', $_POST['adjustment_account']);
 }
 elseif (is_fixed_asset($_POST['mb_flag'])) 
 {
+	gl_all_accounts_list_row(_("Sales Account:"), 'sales_account', $_POST['sales_account']);
 	gl_all_accounts_list_row(_("Asset account:"), 'inventory_account', $_POST['inventory_account']);
 	gl_all_accounts_list_row(_("Depreciation cost account:"), 'cogs_account', $_POST['cogs_account']);
 	gl_all_accounts_list_row(_("Depreciation/Disposal account:"), 'adjustment_account', $_POST['adjustment_account']);
@@ -242,9 +242,12 @@ elseif (is_fixed_asset($_POST['mb_flag']))
 elseif (is_crypto_asset($_POST['mb_flag']))
 {
 	gl_all_accounts_list_row(_("Inventory Account:"), 'inventory_account', $_POST['inventory_account']);
+	hidden('inventory_account', $_POST['inventory_account']);
+	hidden('adjustment_account', $_POST['adjustment_account']);
 }
 else
 {
+	gl_all_accounts_list_row(_("Sales Account:"), 'sales_account', $_POST['sales_account']);
 	gl_all_accounts_list_row(_("Inventory Account:"), 'inventory_account', $_POST['inventory_account']);
 
 	gl_all_accounts_list_row(_("C.O.G.S. Account:"), 'cogs_account', $_POST['cogs_account']);
